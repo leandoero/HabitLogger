f68957f6fe1261a486b18195dc4b3af3e4d0b639
leandoero@mail.ru <leandoero@mail.ru>
2025-04-08 12:05:23 +0300

    changes made to HabitLoggerDbContext

--- HabitLogger/Data/HabitLoggerDbContext.cs
+++ HabitLogger/Data/HabitLoggerDbContext.cs
@@ -13,28 +13,5 @@ namespace HabitLogger.Data
         public HabitLoggerDbContext(DbContextOptions<HabitLoggerDbContext> options) : base(options) { }
 
         public DbSet<Habit> Habits { get; set; }
-
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            //seed data for Difficulties
-            //easy, medium, hard
-
-            var habits = new List<Habit>()
-            {
-                new Habit()
-                {
-                    Id = Guid.Parse("c7666d67-56da-410f-8ab2-16a7fff82747"),
-                    Title = "adad",
-                    CreatedTime = new DateTime(2023, 4, 8),
-                    IsCompleted = false
-                }
-
-            };
-            modelBuilder.Entity<Habit>().HasData(habits);
-
-        }
     }
 }
